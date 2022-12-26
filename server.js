4f0929934146ee960a3446bf42e13aa19f69e4e6
davidgamaserrate1 <david.gama@ufms.br>
2022-12-26 19:58:50 -0300

    feat: server listening

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-require('express')
+const http = require("http")
+const port = 3000;
+
+const server =http.createServer((req, res)=> {
+    res.writeHead(200, {'Content-Type':'text/plain'})
+    res.end('Curso Node');
+})
+
+server.listen(port, ()=>{
+    console.log(`Server listening on port http://localhost:${port}` )
+})
