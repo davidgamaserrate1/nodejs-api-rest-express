20a322deb3290cbda64fbeb70b092c6973e8a980
davidgamaserrate1 <david.gama@ufms.br>
2022-12-26 21:04:26 -0300

    feat: nodemon

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const http = require("http")
 const port = 3000;
 
+
+
+const rotes =
+{
+    '/':'Node Course',
+    '/books':'Book page',
+    '/author':'list of authors',
+}
+    
+
 const server =http.createServer((req, res)=> {
     res.writeHead(200, {'Content-Type':'text/plain'})
-    res.end('Curso Node');
+    res.end(rotes[req.url]);
 })
 
 server.listen(port, ()=>{
